c9274c1990486a3ebbd1c4c6623de9eaa6b19581
박지호 <businessj1ho@gmail.com>
2024-11-05 14:00:40 +0900

    JS 오류 수정

--- front_workspace/03_JS/js/03_연산자예제1.js
+++ front_workspace/03_JS/js/03_연산자예제1.js
@@ -3,14 +3,16 @@ const number2 = document.getElementById("input2");
 const number3 = document.getElementById("input3");
 const result = document.getElementById("calcResult");
 
-const value1 = number1.value;
-const value2 = number2.value;
-const value3 = number3.value;
-
 function plus() {
+  const value1 = number1.value;
+  const value2 = number2.value;
+  const value3 = number3.value;
   result.innerText = Number(value1) + Number(value2) + Number(value3);
 }
 
 function multi() {
+  const value1 = number1.value;
+  const value2 = number2.value;
+  const value3 = number3.value;
   result.innerText = Number(value1) * Number(value2) * Number(value3);
 }

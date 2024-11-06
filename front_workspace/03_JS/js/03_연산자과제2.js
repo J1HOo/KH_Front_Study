000682e9a208a858e4f8c87ebcc2f3ad63ff3d37
박지호 <businessj1ho@gmail.com>
2024-11-06 17:56:46 +0900

    JS 과제 수정

--- front_workspace/03_JS/js/03_연산자과제2.js
+++ front_workspace/03_JS/js/03_연산자과제2.js
@@ -2,6 +2,9 @@ const inputId = document.getElementById("inputId");
 const inputPw = document.getElementById("inputPw");
 const inputMp = document.getElementById("inputMp");
 const inputTp = document.getElementById("inputTp");
+const loginId = document.getElementById("loginId");
+const loginPw = document.getElementById("loginPw");
+const msg = document.getElementById("msg");
 
 function signUp() {
   const id = inputId.value;
@@ -9,5 +12,12 @@ function signUp() {
   const mp = inputMp.value;
   const tp = inputTp.value;
 
-  id && pw && mp && tp ? alert("로그인 성공") : alert("로그인 실패");
+  id && pw && (mp || tp) ? alert("회원가입 성공") : alert("회원가입 실패");
+  mp || tp ? (msg.style.display = "none") : (msg.style.display = "block");
+}
+
+function logIn() {
+  const id = loginId.value;
+  const pw = loginPw.value;
+  id == "123" && pw == "123" ? alert("로그인 성공") : alert("로그인 실패");
 }

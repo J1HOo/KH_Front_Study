714a1c6daae6be8dcd8be3275092af08804c5407
박지호 <businessj1ho@gmail.com>
2024-11-07 15:01:23 +0900

    JS 조건문 예제

--- front_workspace/03_JS/js/06_조건문예제4.js
+++ front_workspace/03_JS/js/06_조건문예제4.js
@@ -1,21 +1,23 @@
 function checkGrade() {
   const inputScore = document.getElementById("inputScore").value;
   const gradeMsg = document.getElementById("gradeMsg");
+  // input이 아닌 p, span, h1 같은 태그들은 value 값이 없음
 
+  gradeMsg.innerText = "등급 :";
   if (inputScore >= 90) {
-    gradeMsg.innerText = "A 등급 입니다.";
+    gradeMsg.innerText += "A"; //등급 :  이라는 글자에 A를 붙이겠다.
     gradeMsg.className = "A";
   } else if (inputScore >= 80) {
-    gradeMsg.innerText = "B 등급 입니다.";
+    gradeMsg.innerText += "B";
     gradeMsg.className = "B";
   } else if (inputScore >= 70) {
-    gradeMsg.innerText = "C 등급 입니다.";
+    gradeMsg.innerText += "C";
     gradeMsg.className = "C";
   } else if (inputScore >= 60) {
-    gradeMsg.innerText = "D 등급 입니다.";
+    gradeMsg.innerText += "D";
     gradeMsg.className = "D";
   } else {
-    gradeMsg.innerText = "F 등급 입니다.";
+    gradeMsg.innerText += "F";
     gradeMsg.className = "F";
   }
 }
---
+++ front_workspace/03_JS/js/06_조건문예제5.js
@@ -0,0 +1,21 @@
+function checkSeason() {
+  const month = document.getElementById("month").value;
+  const seasonMsg = document.getElementById("seasonMsg");
+
+  if (month < 1 || month > 12) {
+    seasonMsg.innerText = "1에서 12사이의 숫자를 입력하세요";
+    seasonMsg.className = "";
+  } else if (month == 12 || month == 1 || month == 2) {
+    seasonMsg.innerText = "겨울입니다.";
+    seasonMsg.className = "winter";
+  } else if (month >= 9) {
+    seasonMsg.innerText = "가을입니다.";
+    seasonMsg.className = "fall";
+  } else if (month >= 6) {
+    seasonMsg.innerText = "여름입니다.";
+    seasonMsg.className = "summer";
+  } else if (month >= 3) {
+    seasonMsg.innerText = "봄입니다.";
+    seasonMsg.className = "spring";
+  }
+}

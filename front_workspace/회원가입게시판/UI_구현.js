f85ee2e3a31267f273870595e708ff40d6806f69
박지호 <businessj1ho@gmail.com>
2024-11-11 18:17:58 +0900

    시험 공부

---
+++ front_workspace/회원가입게시판/UI_구현.js
@@ -0,0 +1,19 @@
+function showHint() {
+  const hint = document.getElementById("hint");
+  hint.style.display = "block";
+}
+
+function hiddenHint() {
+  const hint = document.getElementById("hint");
+  hint.style.display = "none";
+}
+
+function showIdHint() {
+  const hint = document.getElementById("idHint");
+  hint.style.display = "block";
+}
+
+function hiddenIdHint() {
+  const hint = document.getElementById("idHint");
+  hint.style.display = "none";
+}
